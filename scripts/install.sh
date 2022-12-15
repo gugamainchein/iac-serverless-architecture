52fafe10ffc50b7a55f3ca86b74966a5656eb2f7
Raul Soares - Ubuntu <raulrodriguessoares@gmail.com>
2022-12-15 17:27:52 -0300

    feat: Shell script to install terraform

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Installing terraform on the machine"
+
+sleep 5
+
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install terraform
